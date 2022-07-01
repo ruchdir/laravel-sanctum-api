bcf5e8fd2f22e97c4d714d41390ee6fd720312fa
ruchdir <ruchdir@bfsasia.com>
2022-07-01 15:26:00 +0700

    Update to Resource Route

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/products', [ProductController::class, 'index']);
+//? Check your routes : php artisan route:list
+Route::resource('products', ProductController::class);
 
-Route::post('/products', [ProductController::class, 'store']);
+// Route::get('/products', [ProductController::class, 'index']);
+
+// Route::post('/products', [ProductController::class, 'store']);
+
+// Route::get('/products/:id', [ProductController::class, 'show']);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
